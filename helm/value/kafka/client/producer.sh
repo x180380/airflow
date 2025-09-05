eb8e6653a4bb75cc327b24c8d0bc53983ea45593
Li Xinlong <li.xinlong1@presight.ai>
2025-09-05 16:38:47 +0400

    deployment

--- helm/value/kafka/client/producer.sh
+++ helm/value/kafka/client/producer.sh
@@ -1,1 +1,1 @@
-kafka-console-producer.sh --producer.config /tmp/client/client.properties --topic demo --bootstrap-server kafka-controller-headless:9094
+kafka-console-producer.sh --producer.config client.properties --topic demo --bootstrap-server kafka-controller-headless:9094
